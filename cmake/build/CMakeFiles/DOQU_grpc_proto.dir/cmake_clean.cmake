20a2bcb2bde8db207a6a17044dabab49b0e0f71c
congyu711 <sxlxcsxlxc@gmail.com>
2023-03-07 21:20:15 +0800

    first commit

---
+++ cmake/build/CMakeFiles/DOQU_grpc_proto.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DOQU_grpc_proto.dir/DO_and_QU.grpc.pb.cc.o"
+  "CMakeFiles/DOQU_grpc_proto.dir/DO_and_QU.grpc.pb.cc.o.d"
+  "CMakeFiles/DOQU_grpc_proto.dir/DO_and_QU.pb.cc.o"
+  "CMakeFiles/DOQU_grpc_proto.dir/DO_and_QU.pb.cc.o.d"
+  "CS1_and_CS2.grpc.pb.cc"
+  "CS1_and_CS2.grpc.pb.h"
+  "CS1_and_CS2.pb.cc"
+  "CS1_and_CS2.pb.h"
+  "DO_and_CS1.grpc.pb.cc"
+  "DO_and_CS1.grpc.pb.h"
+  "DO_and_CS1.pb.cc"
+  "DO_and_CS1.pb.h"
+  "DO_and_CS2.grpc.pb.cc"
+  "DO_and_CS2.grpc.pb.h"
+  "DO_and_CS2.pb.cc"
+  "DO_and_CS2.pb.h"
+  "DO_and_QU.grpc.pb.cc"
+  "DO_and_QU.grpc.pb.h"
+  "DO_and_QU.pb.cc"
+  "DO_and_QU.pb.h"
+  "QU_and_CS1.grpc.pb.cc"
+  "QU_and_CS1.grpc.pb.h"
+  "QU_and_CS1.pb.cc"
+  "QU_and_CS1.pb.h"
+  "libDOQU_grpc_proto.a"
+  "libDOQU_grpc_proto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DOQU_grpc_proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
